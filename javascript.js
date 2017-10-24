f0c193e8da5c9299d190c0d98db4592c4ae12435
Mateus Garcia <mateuspgx5@live.com>
2017-10-24 12:27:57 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
 
+var offset = $('#MeuMenu').offset().top;
+var $meuMenu = $('#MeuMenu');
+$(document).on('scroll', function () {
+    if (offset <= $(window).scrollTop()) {
+        $meuMenu.addClass('fixar');
+    } else {
+        $meuMenu.removeClass('fixar');
+    }
+});
