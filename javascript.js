f005f67dae78aa794ebfef4e1f3a26f8978de107
Mateus Garcia <mateuspgx5@live.com>
2017-10-24 12:35:29 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@
 window.onscroll = function(){
    var top = window.pageYOffset || document.documentElement.scrollTop
    if( top > 300 ) {
-       alert('Maior que 300');
+       console.log('Maior que 300');
+       document.getElementById("MeuMenu").classList.add('fixar');
    }
 }
